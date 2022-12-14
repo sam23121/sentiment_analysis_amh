8994c6412243c37872b7aae6c4eefc5fbe25031b
sam <smlalene@gmail.com>
2022-12-14 14:22:01 +0300

    first commit

---
+++ setiment analysis/scripts/cleaning.py
@@ -0,0 +1,100 @@
+import re
+
+def remove_punc_and_special_chars(ls): 
+    new_ls = []
+    for text in ls:
+        text = str(text)
+        normalized_text = re.sub('[\!\@\#\$\%\^\«\»\&\*\(\)\…\[\]\{\}\;\“\”\›\’\‘\"\'\:\,\.\‹\/\<\>\?\\\\|\`\´\~\-\=\+\፡\።\፤\;\፦\፥\፧\፨\፠\፣]', '',text)
+        new_ls.append(normalized_text)
+    return new_ls
+
+def remove_ascii_and_numbers(ls):
+    new_ls = []
+    for text_input in ls:
+        text_input = str(text_input)
+        rm_num_and_ascii=re.sub('[A-Za-z0-9]','',text_input)
+        text = re.sub('[\'\u1369-\u137C\']+','',rm_num_and_ascii)
+        new_ls.append(text)
+    return new_ls
+
+def normalize_char_level_missmatch(ls):
+        new_ls = []
+        for input_token in ls:
+                input_token = str(input_token)
+                rep1=re.sub('[ሃኅኃሐሓኻ]','ሀ',input_token)
+                rep2=re.sub('[ሑኁዅ]','ሁ',rep1)
+                rep3=re.sub('[ኂሒኺ]','ሂ',rep2)
+                rep4=re.sub('[ኌሔዄ]','ሄ',rep3)
+                rep5=re.sub('[ሕኅ]','ህ',rep4)
+                rep6=re.sub('[ኆሖኾ]','ሆ',rep5)
+                rep7=re.sub('[ሠ]','ሰ',rep6)
+                rep8=re.sub('[ሡ]','ሱ',rep7)
+                rep9=re.sub('[ሢ]','ሲ',rep8)
+                rep10=re.sub('[ሣ]','ሳ',rep9)
+                rep11=re.sub('[ሤ]','ሴ',rep10)
+                rep12=re.sub('[ሥ]','ስ',rep11)
+                rep13=re.sub('[ሦ]','ሶ',rep12)
+                rep14=re.sub('[ዓኣዐ]','አ',rep13)
+                rep15=re.sub('[ዑ]','ኡ',rep14)
+                rep16=re.sub('[ዒ]','ኢ',rep15)
+                rep17=re.sub('[ዔ]','ኤ',rep16)
+                rep18=re.sub('[ዕ]','እ',rep17)
+                rep19=re.sub('[ዖ]','ኦ',rep18)
+                rep20=re.sub('[ጸ]','ፀ',rep19)
+                rep21=re.sub('[ጹ]','ፁ',rep20)
+                rep22=re.sub('[ጺ]','ፂ',rep21)
+                rep23=re.sub('[ጻ]','ፃ',rep22)
+                rep24=re.sub('[ጼ]','ፄ',rep23)
+                rep25=re.sub('[ጽ]','ፅ',rep24)
+                rep26=re.sub('[ጾ]','ፆ',rep25)
+                #Normalizing words with Labialized Amharic characters such as በልቱዋል or  በልቱአል to  በልቷል  
+                rep27=re.sub('(ሉ[ዋአ])','ሏ',rep26)
+                rep28=re.sub('(ሙ[ዋአ])','ሟ',rep27)
+                rep29=re.sub('(ቱ[ዋአ])','ቷ',rep28)
+                rep30=re.sub('(ሩ[ዋአ])','ሯ',rep29)
+                rep31=re.sub('(ሱ[ዋአ])','ሷ',rep30)
+                rep32=re.sub('(ሹ[ዋአ])','ሿ',rep31)
+                rep33=re.sub('(ቁ[ዋአ])','ቋ',rep32)
+                rep34=re.sub('(ቡ[ዋአ])','ቧ',rep33)
+                rep35=re.sub('(ቹ[ዋአ])','ቿ',rep34)
+                rep36=re.sub('(ሁ[ዋአ])','ኋ',rep35)
+                rep37=re.sub('(ኑ[ዋአ])','ኗ',rep36)
+                rep38=re.sub('(ኙ[ዋአ])','ኟ',rep37)
+                rep39=re.sub('(ኩ[ዋአ])','ኳ',rep38)
+                rep40=re.sub('(ዙ[ዋአ])','ዟ',rep39)
+                rep41=re.sub('(ጉ[ዋአ])','ጓ',rep40)
+                rep42=re.sub('(ደ[ዋአ])','ዷ',rep41)
+                rep43=re.sub('(ጡ[ዋአ])','ጧ',rep42)
+                rep44=re.sub('(ጩ[ዋአ])','ጯ',rep43)
+                rep45=re.sub('(ጹ[ዋአ])','ጿ',rep44)
+                rep46=re.sub('(ፉ[ዋአ])','ፏ',rep45)
+                rep47=re.sub('[ቊ]','ቁ',rep46) #ቁ can be written as ቊ
+                rep48=re.sub('[ኵ]','ኩ',rep47) #ኩ can be also written as ኵ  
+                new_ls.append(rep48)
+        return new_ls
+
+def remove_unexpected(new_ls):
+    new_ls2 = []
+    for text in new_ls:
+        text = re.sub("[\ufeff]",'',text)
+        new_ls2.append(text)
+    
+    return new_ls2
+
+def pad_features(reviews_int, seq_length):
+    ''' Return features of review_ints, where each review is padded with 0's or truncated to the input seq_length.
+    '''
+    features = np.zeros((len(reviews_int), seq_length), dtype = int)
+    
+    for i, review in enumerate(reviews_int):
+        review_len = len(review)
+        
+        if review_len <= seq_length:
+            zeroes = list(np.zeros(seq_length-review_len))
+            new = zeroes+review
+        elif review_len > seq_length:
+            new = review[0:seq_length]
+        
+        features[i,:] = np.array(new)
+    
+    return features
